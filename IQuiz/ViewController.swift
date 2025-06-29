ae146f141cade88ea5580ce729d72aff23deeb2d
NavarrosDev <gabrielnavarro.dev@gmail.com>
2025-06-29 00:44:11 -0300

    Initial Commit

---
+++ IQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IQuiz
+//
+//  Created by Gabriel Navarro on 29/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
